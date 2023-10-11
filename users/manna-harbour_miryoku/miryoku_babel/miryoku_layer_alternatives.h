9d09d452cd93a7f8ce6a1216340345ea518cb93d
Jesse Bao <github@jessebao.com>
2023-10-11 13:04:22 -0500

    Update miryoku_layer_alternatives.h

--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -42,7 +42,7 @@ U_NP,              U_NP,              U_NA,              U_NA,              U_NA
 
 #define MIRYOKU_ALTERNATIVES_MOUSE \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             KC_PSCR,           TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
-KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_SCRL,           DRAG_SCROLL,       DRAG_SCROLL,       DRAG_SCROLL,       DRAG_SCROLL,           \
+KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_SCRL,           DRGSCRL,       DRGSCRL,       DRGSCRL,       DRGSCRL,           \
 KC_WH_L,           KC_WH_U,           KC_WH_D,           KC_WH_R,           U_NA,              KC_PAUS,           TD(U_TD_U_MOUSE),  TD(U_TD_U_SYM),    KC_ALGR,           U_NA,              \
 U_NP,              U_NP,              KC_BTN3,           KC_BTN1,           KC_BTN2,           KC_ENT,            KC_SPC,            KC_DEL,            U_NP,              U_NP
 
