8a6f5c12cf34185400ad94efb9e07c5b6b152492
Jesse Bao <github@jessebao.com>
2023-10-11 12:58:31 -0500

    Update miryoku_layer_alternatives.h

--- users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
+++ users/manna-harbour_miryoku/miryoku_babel/miryoku_layer_alternatives.h
@@ -37,12 +37,12 @@ U_NP,              U_NP,              KC_ESC,            KC_BSPC,           KC_T
 TD(U_TD_BOOT),     TD(U_TD_U_TAP),    TD(U_TD_U_EXTRA),  TD(U_TD_U_BASE),   U_NA,              U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              CW_TOGG,           KC_LEFT,           KC_DOWN,           KC_UP,             KC_RGHT,           \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_NA,              KC_INS,            KC_HOME,           KC_PGDN,           KC_PGUP,           KC_END,            \
-U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_SPC,            KC_DEL,            U_NP,              U_NP
+U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              KC_ENT,            KC_DEL,            KC_DEL,            U_NP,              U_NP
 
 
 #define MIRYOKU_ALTERNATIVES_MOUSE \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             KC_PSCR,           TD(U_TD_U_BASE),   TD(U_TD_U_EXTRA),  TD(U_TD_U_TAP),    TD(U_TD_BOOT),     \
-KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_SCRL,           KC_LSFT,           KC_LCTL,           KC_LALT,           KC_LGUI,           \
+KC_LGUI,           KC_LALT,           KC_LCTL,           KC_LSFT,           U_NA,              KC_SCRL,           DRAG_SCROLL,       DRAG_SCROLL,       DRAG_SCROLL,       DRAG_SCROLL,           \
 KC_WH_L,           KC_WH_U,           KC_WH_D,           KC_WH_R,           U_NA,              KC_PAUS,           TD(U_TD_U_MOUSE),  TD(U_TD_U_SYM),    KC_ALGR,           U_NA,              \
 U_NP,              U_NP,              KC_BTN3,           KC_BTN1,           KC_BTN2,           KC_ENT,            KC_SPC,            KC_DEL,            U_NP,              U_NP
 
