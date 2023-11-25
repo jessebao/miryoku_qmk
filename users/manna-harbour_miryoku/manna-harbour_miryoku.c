d9e8b02a350c462aa62d3dbf7a41d8a774dccd8a
Jesse Bao <github@jessebao.com>
2023-11-25 01:04:01 -0600

    Update manna-harbour_miryoku.c

--- users/manna-harbour_miryoku/manna-harbour_miryoku.c
+++ users/manna-harbour_miryoku/manna-harbour_miryoku.c
@@ -58,10 +58,10 @@ const key_override_t **key_overrides = (const key_override_t *[]){
     NULL
 };
 
-// void pointing_device_init_user(void) {
-//     set_auto_mouse_layer(U_MOUSE); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
-//     set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
-// }
+void pointing_device_init_user(void) {
+    set_auto_mouse_layer(U_MOUSE); // only required if AUTO_MOUSE_DEFAULT_LAYER is not set to index of <mouse_layer>
+    set_auto_mouse_enable(true);         // always required before the auto mouse feature will work
+}
 
 // thumb combos
 
